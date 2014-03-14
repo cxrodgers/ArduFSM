bbb3a48eae240275fd535ceea74b98b19e141c48
Chris Rodgers <xrodgers@gmail.com>
2014-03-13 20:46:38 -0400

    session params settable by user; dual lickport states; touched announcing

--- LickTrain/LickTrain.ino
+++ LickTrain/LickTrain.ino
@@ -20,29 +20,51 @@ struct PINS_TYPE
 } PINS;
 
 // States
-struct STATE_TYPE
+enum STATE_TYPE
 {
-  static const int TRIAL_ANNOUNCE = 0;
-  static const int WAIT = 1;
-  static const int REWARD = 2;
-  static const int INTER_TRIAL = 3;
-} STATES;
+  TRIAL_START,
+  WAIT_L,
+  WAIT_R,
+  REWARD_L,
+  REWARD_R,
+  REWARD_TIMER_L,
+  REWARD_TIMER_R,
+  START_INTER_TRIAL_INTERVAL,
+  INTER_TRIAL_INTERVAL
+} current_state = TRIAL_START;
 
+// Trial variables
+struct TRIAL_PARAMS_TYPE
+{
+  char rewarded_side = 'L';
+} current_trial_params;
+
+// Session params -- combine this with trial variables
+struct SESSION_PARAMS_TYPE
+{
+  char force = 'X';
+  unsigned long inter_trial_interval = 1000;
+  unsigned long reward_dur_l = 40;
+  unsigned long reward_dur_r = 50;  
+} session_params;
 
 // Debugging announcements
 unsigned long speak_at = 1000;
 unsigned long interval = 1000;
 
-// State machine global
-unsigned int current_state = 0;
-
 // State machine timers
 unsigned long reward_timer = 0;
+unsigned long iti_timer = 0;
 
+// touched monitor
+uint16_t sticky_touched = 0;
+
+
+int set_session_params(SESSION_PARAMS_TYPE &session_params, String cmd);
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Running setup.");
 
   // MPR121 touch sensor setup
@@ -68,32 +90,73 @@ void setup()
 void loop()
 {
   unsigned long time = millis();
-  int next_state = current_state;
+  STATE_TYPE next_state = current_state;
   uint16_t touched = 0;
 
   // Announce the time
   if (time > speak_at)
   {
-    Serial.println((String) "DEBUG The time is " + time);
+    Serial.println((String) "DEBUG time " + time);
     speak_at += interval;
   }
 
   // Receive any chat
   String received_chat;
   received_chat = receive_chat();
+  
+  // Set session variables
+  int status = set_session_params(session_params, received_chat);
+  if (status == 1)
+    Serial.println((String) "DEBUG cannot parse " + received_chat);
+  
 
   // state
   switch (current_state)
   {
-    case 0:
+    case TRIAL_START:
+      // Set trial variables
+      switch (session_params.force)
+      {
+        case 'L':
+          current_trial_params.rewarded_side = 'L';
+          break;
+        case 'R':
+          current_trial_params.rewarded_side = 'R';
+          break;
+        case 'X':
+          // should choose randomly
+          current_trial_params.rewarded_side = 'L';
+          break;
+      }
+
       // Trial start
       Serial.println((String) "TRIAL START " + time);
-      next_state = 1;
+      Serial.println((String) "TRIAL SIDE " + 
+        current_trial_params.rewarded_side);
+    
+      // Set next state
+      switch (current_trial_params.rewarded_side)
+      {
+        case 'L':
+          next_state = WAIT_L;
+          break;
+        case 'R':
+          next_state = WAIT_R;
+          break;
+      }
+
       break;
     
-    case 1:
+    case WAIT_L:
       // Poll touch inputs
       touched = pollTouchInputs();
+      
+      // announce sticky
+      if (touched != sticky_touched)
+      {
+        Serial.println((String) time + " EVENT TOUCHED " + touched);
+        sticky_touched = touched;
+      }
     
       // Check chat
       if (received_chat.length() > 0)
@@ -102,38 +165,112 @@ void loop()
       }      
 
       // transition if received REWARD or touched
-      if (received_chat.equals("REWARD") || (touched > 0))
+      if (received_chat.equals("REWARD"))
       {
-        if (touched > 0)
-          Serial.println("TRIAL OUTCOME HIT");
-        else
-          Serial.println("TRIAL OUTCOME SPOIL");
-        next_state = 2;
+        Serial.println("TRIAL OUTCOME SPOIL");
+        next_state = REWARD_L;
+      }
+      
+      if ((get_touched_channel(touched, 0) == 1) && 
+          (get_touched_channel(touched, 1) == 0))
+      {
+        Serial.println("TRIAL OUTCOME HIT");
+        //~ Serial.println((String) "DEBUG touched = " + (String) touched);
+        next_state = REWARD_L;
+      }
+      break;
+
+    case WAIT_R:
+      // Poll touch inputs
+      touched = pollTouchInputs();
+
+      // announce sticky
+      if (touched != sticky_touched)
+      {
+        Serial.println((String) time + " EVENT TOUCHED " + touched);
+        sticky_touched = touched;
+      }
+    
+      // Check chat
+      if (received_chat.length() > 0)
+      {
+        received_chat.trim();
       }      
+
+      // transition if received REWARD or touched
+      if (received_chat.equals("REWARD"))
+      {
+        Serial.println("TRIAL OUTCOME SPOIL");
+        next_state = REWARD_R;
+      }
+      
+      if ((get_touched_channel(touched, 1) == 1) && 
+          (get_touched_channel(touched, 0) == 0))
+      {
+        Serial.println("TRIAL OUTCOME HIT");
+        //~ Serial.println((String) "DEBUG touched = " + (String) touched);
+        next_state = REWARD_R;
+      }
       break;
       
-    case 2:
+    case REWARD_L:
       // Rewarding state
       // Chats and touches are ignored!
       // start a timer
-      Serial.println((String) "REWARD DELIVERED " + time);
-      reward_timer = time + 40;
-    
+      Serial.println((String) time + " EVENT REWARD_L");
+      reward_timer = time + session_params.reward_dur_l;
       // rewarding
       digitalWrite(6, HIGH);    // open solenoid valve
     
       // transition
-      next_state = 3;
+      next_state = REWARD_TIMER_L;
+      break;
+
+    case REWARD_R:
+      // Rewarding state
+      // Chats and touches are ignored!
+      // start a timer
+      Serial.println((String) time + " EVENT REWARD_R");
+      reward_timer = time + session_params.reward_dur_r;
+    
+      // rewarding
+      digitalWrite(7, HIGH);    // open solenoid valve
+    
+      // transition
+      next_state = REWARD_TIMER_R;
       break;
     
-    case 3:
+    case REWARD_TIMER_L:
       // Waiting state
       // Chats and touches are ignored!
       // wait for timer
       if (time >= reward_timer)
       {
-        next_state = 0;
+        next_state = START_INTER_TRIAL_INTERVAL;
         digitalWrite(6, LOW);
+      }
+      break;
+
+    case REWARD_TIMER_R:
+      // Waiting state
+      // Chats and touches are ignored!
+      // wait for timer
+      if (time >= reward_timer)
+      {
+        next_state = START_INTER_TRIAL_INTERVAL;
+        digitalWrite(7, LOW);
+      }
+      break;
+    
+    case START_INTER_TRIAL_INTERVAL:
+      iti_timer = time + session_params.inter_trial_interval;
+      next_state = INTER_TRIAL_INTERVAL;
+      break;
+    
+    case INTER_TRIAL_INTERVAL:
+      if (time >= iti_timer)
+      {
+        next_state = TRIAL_START;
       }
       break;
   }
@@ -145,3 +282,55 @@ void loop()
     current_state = next_state;
   }
 }
+
+
+int get_touched_channel(uint16_t touched, unsigned int i)
+{
+  return (touched & (1 << i)) >> i;
+}
+
+int set_session_params(SESSION_PARAMS_TYPE &session_params, String cmd)
+{ /* Update the trial parameters with user requests 
+  
+  Returns 1 if cannot parse command
+  */
+  int status = 0;
+  
+  // If sent FORCE L or FORCE R, then set session_params accordingly
+  if ((cmd.substring(0, 5) == "FORCE"))
+  {
+    if (cmd.substring(5, 7) == " L")
+    {
+      session_params.force = 'L';
+    }
+    else if (cmd.substring(5, 7) == " R")
+    {
+      session_params.force = 'R';
+    }
+    else
+    {
+      // Cannot parse command
+      status = 1;
+    }
+  }
+  
+  
+  if ((cmd.substring(0, 15) == "SET REWARD_DUR_"))
+  {
+    if (cmd.substring(15, 16) == "L")
+    {
+      session_params.reward_dur_l = cmd.substring(17).toInt();
+    }
+    else if (cmd.substring(15, 16) == "R")
+    {
+      session_params.reward_dur_r = cmd.substring(17).toInt();
+    }
+    else
+    {
+      // Cannot parse command
+      status = 1;
+    }
+  }
+  
+  return status;
+}   
