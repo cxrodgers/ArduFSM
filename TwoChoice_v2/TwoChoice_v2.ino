b1d2de4e9e1f6d24421e29f80299367dba734975
Chris Rodgers <xrodgers@gmail.com>
2014-09-12 00:40:23 -0400

    comments on waiting state

--- TwoChoice_v2/TwoChoice_v2.ino
+++ TwoChoice_v2/TwoChoice_v2.ino
@@ -15,6 +15,38 @@ Here are the things that the user should have to change for each protocol:
 * User-defined states in switch statement
 * param_abbrevs, param_values, tpidx_*, N_TRIAL_PARAMS
 
+
+Template for a waiting state. Try to make it match this syntax.
+case A_WAITING_STATE:
+  // Wait the specified amount of time
+  if (state_timer == -1)
+  {
+    // Start timer and run first-time code
+    state_timer = time + WAITING_TIME;
+
+    a_waiting_state_run_once();
+  }
+  else
+  {
+    a_waiting_state_run_many_times();
+  }
+  
+  if (time > state_timer)
+  {
+    a_waiting_state_run_when_done();
+    
+    // Check timer and run every-time code
+    next_state = NEXT_STATE;
+    state_timer = -1;
+  }
+  break;
+
+Eventually this could be some kind of object:
+case A_WAITING_STATE:
+  waiting_state_obj.run();
+
+Where the object derives from something that implements the basic pattern
+above, and the user just filles in the run_once(), run_many_times(), etc
 */
 #include "chat.h"
 #include "hwconstants.h"
