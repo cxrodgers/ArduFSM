27be9ff48fb33eb57190bbf528282746dd30a6fd
Chris Rodgers <xrodgers@gmail.com>
2014-09-14 19:39:16 -0400

    adding header files for TimedState

---
+++ TwoChoice_v2/TimedState.cpp
@@ -0,0 +1,22 @@
+#include "TimedState.h"
+
+void TimedState::run(unsigned long time, uint16_t touched)
+{
+  // boiler plate timer code
+  if (timer == 0)
+  {
+    s_setup();
+    flag_stop = 0;
+    timer = time + duration;
+  }
+  
+  if (flag_stop || (time >= timer))
+  {
+    s_finish();
+    timer = 0;      
+  }
+  else
+  {
+    loop(touched);
+  }
+};
---
+++ TwoChoice_v2/TimedState.h
@@ -0,0 +1,52 @@
+#include "Arduino.h"
+/*
+
+Template for a waiting state. Try to make it match this syntax.
+case A_WAITING_STATE:
+  // Wait the specified amount of time
+  if (state_timer == -1)
+  {
+    // Start timer and run first-time code
+    state_timer = time + WAITING_TIME;
+
+    a_waiting_state_run_once();
+  }
+  else
+  {
+    a_waiting_state_run_many_times();
+  }
+  
+  if (time > state_timer)
+  {
+    a_waiting_state_run_when_done();
+    
+    // Check timer and run every-time code
+    next_state = NEXT_STATE;
+    state_timer = -1;
+  }
+  break;
+
+Eventually this could be some kind of object:
+case A_WAITING_STATE:
+  waiting_state_obj.run();
+
+Where the object derives from something that implements the basic pattern
+above, and the user just filles in the run_once(), run_many_times(), etc
+*/
+
+class TimedState
+{
+  protected:
+    unsigned long timer = 0;
+    unsigned long duration = 0;
+    bool flag_stop = 0;
+    virtual void s_setup();
+    virtual void loop(uint16_t touched);
+    virtual void s_finish();
+  
+  public:
+    TimedState(unsigned long t, unsigned long d) : timer(t), duration(d) { };
+    void run(unsigned long time, uint16_t touched);
+};
+
+
