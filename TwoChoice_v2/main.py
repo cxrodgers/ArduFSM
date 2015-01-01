ee0e44a7849ca34d1aa300e7ec0231d15c1d690c
Chris Rodgers <xrodgers@gmail.com>
2014-12-31 19:40:50 -0500

    updating example main

--- TwoChoice_v2/main.py
+++ TwoChoice_v2/main.py
@@ -9,122 +9,49 @@
 # So, if the timeouts are too low, it spends a lot more time reading the
 # logfile and there is more overhead overall.
 
-
 import sys
-if '/home/chris/dev/ArduFSM/TwoChoice_v2/' not in sys.path:
-    sys.path.append('/home/chris/dev/ArduFSM/TwoChoice_v2/')
-
-import ArduFSM
-import ArduFSM.chat2
-import ArduFSM.plot2
-import TrialSpeak, TrialMatrix
+import os
 import numpy as np, pandas
 import my
 import time
 import curses
+import matplotlib.pyplot as plt
+
+# Ardu imports
+if '/home/chris/dev/ArduFSM/TwoChoice_v2/' not in sys.path:
+    sys.path.append('/home/chris/dev/ArduFSM/TwoChoice_v2/')
+import ArduFSM
+import ArduFSM.chat2
+import ArduFSM.plot2
+import TrialSpeak, TrialMatrix
 import trial_setter_ui
 import Scheduler
 import trial_setter2
-import matplotlib.pyplot as plt
+import mainloop
 
 
+## Find out what rig we're in using the current directory
+this_dir_name = os.getcwd()
+rigname = os.path.split(this_dir_name)[1]
+serial_port = mainloop.get_serial_port(rigname)
+
+## Get params
+params_table = mainloop.get_params_table()
+params_table = mainloop.assign_rig_specific_params(rigname, params_table)
+params_table['current-value'] = params_table['init_val'].copy()
+
+## Get trial types
+trial_types = mainloop.get_trial_types('trial_types_3srvpos')
+
+## Initialize the scheduler
+scheduler = Scheduler.SessionStarter(trial_types=trial_types)
 
 ## Create Chatter
 logfilename = 'out.log'
-logfilename = None # autodate?
-chatter = ArduFSM.chat2.Chatter(to_user=logfilename, baud_rate=115200, 
-    serial_timeout=.1, serial_port='/dev/ttyACM1')
+logfilename = None # autodate
+chatter = ArduFSM.chat2.Chatter(to_user=logfilename, to_user_dir='./logfiles',
+    baud_rate=115200, serial_timeout=.1, serial_port=serial_port)
 logfilename = chatter.ofi.name
-
-## Params
-YES = 3
-NO = 2
-MD = 0
-
-"""Params table
-
-These are all the parameters that the Arduino code uses.
-Columns:
-    name            The abbreviation used to set the param.
-    init_val        The value that the Python code uses by default, if it isn't
-                    set in any other way. If this is MD ("must-define"),
-                    then it must be set by something, either at the beginning
-                    of the session or on each trial.
-    required_ET     If True, then it is required to be set on each trial.
-                    Currently this is ignored, though we should implement
-                    error checking for it on both sides.
-                    init_val should probably be ignored for required_ET,
-                    unless we want to interpret it as some sort of default?
-    reported_ET     If True, then its value is reported by the Arduino on 
-                    each trial, using the TRLP command.
-                    Currently this is hand-copied to the Arduino code.
-                    Eventually, we probably want to default to all params
-                    reported and no params required, and then set it from
-                    Python side.
-    ui_accessible   If True, then the UI provides a mechanism for setting it.
-                    This is simply to make the UI less overwhelming, and
-                    because some params seem unlikely to ever change during
-                    a session.
-    rig_dependent   If True, then this parameter is expected to vary by rig.
-                    A rig-specific params file will be loaded and used to
-                    set this param. If it is not specified by that file,
-                    its init_val is used (unless it is MD).
-    send_on_init    If True, then this parameter is sent before the first
-                    trial begins. Any param that is not send_on_init and
-                    not required_ET will use Arduino defaults, which really
-                    only applies to params that we don't expect to ever change.
-
-Note that the value 0 may never be sent to the Arduino, due to limitations
-in the Arudino-side conversion of strings to int. To send boolean values,
-use YES and NO. It is undetermined whether negative values should be allowed.
-
-We currently define MD as 0, since this is not an allowable value to send.
-"""
-params_table = pandas.DataFrame([
-    ('STPPOS',  MD,       1, 1, 0, 0, 0),
-    ('RWSD',    MD,       1, 1, 0, 0, 0),
-    ('SRVPOS',  MD,       1, 1, 0, 0, 0),
-    ('ISRND',   NO,       1, 1, 0, 0, 0),
-    ('RD_L',    MD,       0, 0, 1, 1, 1),
-    ('RD_R',    MD,       0, 0, 1, 1, 1),
-    ('ITI',     50,       0, 0, 1, 0, 1),
-    ('PSW',     1,        0, 0, 1, 0, 0),
-    ('TO',      6000,     0, 0, 1, 0, 1),       
-    ('TOE',     YES,      0, 0, 1, 0, 1),
-    ('MRT',     1,        0, 0, 1, 0, 1),
-    ('STPSPD',  MD,       0, 0, 0, 1, 0),
-    ('STPFR',   50,       0, 0, 0, 1, 0),
-    ('2PSTP',   MD,       0, 0, 0, 1, 1),
-    ('SRVST',   1000,     0, 0, 0, 1, 0),
-    ('STPIP',   50,       0, 0, 0, 1, 1),
-    ('SRVFAR',  1900,     0, 0, 0, 1, 1),
-    ('SRVTT',   MD,       0, 0, 0, 1, 1),
-    ('RWIN',    45000,    0, 0, 0, 0, 1),
-    ('IRI',     500,      0, 0, 0, 0, 0),    
-    ('TOUT',    6,        0, 0, 1, 1, 1),
-    ('RELT',    6,        0, 0, 1, 1, 1),
-    ],
-    columns=('name', 'init_val', 'required_ET', 'reported_ET', 
-        'ui-accessible', 'rig-dependent', 'send_on_init'),
-    ).set_index('name')
-bool_list = ['required_ET', 'reported_ET', 'ui-accessible', 
-    'rig-dependent', 'send_on_init']
-params_table[bool_list] = params_table[bool_list].astype(np.bool)
-
-# Assign the rig-specific by hand for now
-params_table['init_val']['STPSPD'] = 20
-params_table['init_val']['2PSTP'] = NO
-params_table['init_val']['SRVTT'] = 2000
-params_table['init_val']['RD_L'] = 20
-params_table['init_val']['RD_R'] = 25
-
-# Column for current value
-params_table['current-value'] = params_table['init_val'].copy()
-
-## Initialize the scheduler
-#trial_types = pandas.read_pickle('trial_types_2stppos')
-trial_types = pandas.read_csv('trial_types_3srvpos')
-scheduler = Scheduler.RandomStim(trial_types=trial_types)
 
 ## Trial setter
 ts_obj = trial_setter2.TrialSetter(chatter=chatter, 
@@ -169,6 +96,16 @@ try:
 
         # Run the trial setting logic
         translated_trial_matrix = ts_obj.update(splines)
+        
+        ## Meta-scheduler
+        # Not sure how to handle this yet. Probably should be an object
+        # ts_obj.meta_scheduler that can change ts_obj.scheduler, or a method
+        # meta_scheduler(trial_matrix) that returns the new scheduler
+        # Actually, probably makes the most sense to have each meta-scheduler
+        # just be a scheduler called "auto" or something.
+        if len(translated_trial_matrix) > 5:
+            new_scheduler = Scheduler.RandomStim(trial_types=trial_types)
+            ts_obj.scheduler = new_scheduler
         
         ## Update UI
         if RUN_UI:
