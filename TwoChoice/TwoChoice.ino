314f40bb302dc498b59ce87dd2870957812e6dc2
Chris Rodgers <xrodgers@gmail.com>
2015-03-30 22:06:36 -0400

    moving all of ArduFSM into twochoice, but error

--- TwoChoice/TwoChoice.ino
+++ TwoChoice/TwoChoice.ino
@@ -27,7 +27,7 @@ State* user_trial_start()
 #include <Servo.h>
 #include <Stepper.h>
 //#include "States.h"
-#include "ArduFSM.h"
+//#include "ArduFSM.h"
 #include "Arduino.h"
 //#include "Params.h"
 //#include "Actions.h"
@@ -42,7 +42,115 @@ long sticky_stepper_position = 0;
 // Stepper. We won't assign till we know if it's 2pin or 4pin
 Stepper *stimStepper = 0;
 
+bool flag_start_trial = 0;
 
+
+///////// Ardu FSM H
+
+
+//// The value of the unique 'id' field of each state
+// Could think about making this an enum
+#define STATE_ID_WAIT_TO_START_TRIAL 1
+#define STATE_ID_TRIAL_START 2
+#define STATE_ID_FINISH_TRIAL 3
+
+
+//// Basic State definition
+// All states derive from this base class
+class State
+{
+  public:
+    State() { };
+
+    // Each State has a unique "id" field, mainly used in announcing
+    // state changes over the serial port
+    virtual int id() { return 0; }
+    
+    // Every State defines a run method, which returns a pointer to the 
+    // next State.
+    virtual State* run(unsigned long time) = 0;
+    
+};
+
+
+//// Derived TimedState with a duration
+// This provides a unified "run" syntax to handle the duration
+// Derived classes may implement s_setup, loop, s_finish, and update
+// Briefly, on the first "run", the timer is set to be the current time
+// plus the duration and s_setup() is run. Then, and on subsequent runs,
+// loop() is called, unless the timer has elapsed or flag_stop has been set,
+// in which case s_finish() is run.
+// The state can be changed by s_finish(), or even by loop(). The latter would
+// be useful for transitioning to another state while keeping the timer going.
+class TimedState : public State
+{
+  protected:
+    // flags and timers
+    unsigned long time_of_last_call = 0;
+    unsigned long timer = 0;
+    unsigned long duration = 0;
+    bool flag_stop = 0;
+  
+    // virtual functions, to be determined by the derived class
+    virtual void s_setup() { }
+    virtual State* loop() { return this; }
+    virtual State* s_finish() = 0;
+  
+  public:
+    TimedState(long d) : duration(d) { 
+      // All param_values are "long", so we take a long here, even though
+      // a negative duration wouldn't make any sense.
+      if (d < 0) Serial.println("ERR duration <0"); 
+    };
+    
+    // the main run function
+    State* run(unsigned long time);
+    
+    // setter for duration
+    void set_duration(unsigned long new_duration);
+    
+    // generic setters, to be determined by the derived class
+    virtual void update() {};
+};
+
+
+//// Definitions of standard states
+// This one waits to receive the start trial signal
+class StateWaitToStartTrial : public State {
+  protected:
+    State* run(unsigned long time);
+  public:
+    StateWaitToStartTrial() : State() { };
+    int id() { return STATE_ID_WAIT_TO_START_TRIAL; }
+};
+
+// This one runs once when the trial starts
+class StateTrialStart : public State {
+  protected:
+    State* run(unsigned long time);
+  public:
+    StateTrialStart() : State() { };
+    int id() { return STATE_ID_TRIAL_START; }
+};
+
+// This one runs at the end of each trial
+class StateFinishTrial : public TimedState {
+  protected:
+    void s_setup();
+    State* s_finish();
+  public:
+    StateFinishTrial(long d) : TimedState(d) { };
+    int id() { return STATE_ID_FINISH_TRIAL; }    
+};
+
+
+// Utility function to announce state changes at end of each loop
+void announce_state_change(unsigned long time, State *curent_state,
+  State *next_state);
+
+///// END ArduFSM H
+
+State* user_trial_start(unsigned long time);
 
 
 
@@ -473,6 +581,260 @@ void asynch_action_light_on()
 
 /////
 
+
+
+
+
+
+//////////// Ardu FSM CPP
+
+
+
+// Instantiate the standard states
+State* state_wait_to_start_trial = new StateWaitToStartTrial;
+State* state_trial_start = new StateTrialStart;
+State* state_finish_trial = new StateFinishTrial(
+  1000);
+
+
+//// Implementation of derived TimedState
+State* TimedState::run(unsigned long time)
+{ /* Boilerplate 'run' code for a state with a certain length of time.
+    
+  This is called on every pass, as long as the FSM is in this state.
+  Depending on the current state of the timer, flag_stop, and the time, this
+  calls s_setup(), loop(), or s_finish().
+
+  States that inherit from this should define their own s_setup(), loop(),
+  and s_finish().
+    
+  Simple algorithm:
+    * If the timer is set to 0, take this to mean that we haven't started yet.
+      Run s_setup()
+      Set the timer and the flag
+    * If the time is greater than the timer, or the flag_stop has been set,
+      run the s_finish()
+    * Otherwise, run loop()
+  */
+  // always store time of last call
+  State* next_state;
+  time_of_last_call = time;    
+    
+  // boiler plate timer code
+  if (timer == 0)  {
+    s_setup();
+    flag_stop = 0;
+    timer = time + duration;
+  }
+  
+  if (flag_stop || (time >= timer))  {
+    next_state = s_finish();
+    timer = 0;      
+  }
+  else {
+    next_state = loop();
+  }
+  
+  return next_state;
+};
+
+void TimedState::set_duration(unsigned long new_duration) {
+  /* Update the duration of the state, for instance after param change */
+  duration = new_duration;
+}
+
+
+//// Implementations of standard states
+// StateWaitToStartTrial
+// Checks the global flag_start_trial, which is set by communications
+// on every loop. Once the flag is true, issues the TRL_RELEASED token
+// and resets the flag and continues to state_trial_start
+State* StateWaitToStartTrial::run(unsigned long time)
+{
+  // Wait until we receive permission to continue  
+  if (flag_start_trial) {
+    // Announce that we have ended the trial and reset the flag
+    Serial.print(time);
+    Serial.println(" TRL_RELEASED");
+    flag_start_trial = 0;
+    return state_trial_start; // Proceed to next trial
+  }
+  return state_wait_to_start_trial;
+}
+
+// StateTrialStart
+// Announces parameters
+// Sets results to default values
+// Calls user_trial_start
+State* StateTrialStart::run(unsigned long time)
+{ 
+  // Get the values from Params.h
+  long* param_values = get_param_values();
+  long* results_values = get_results_values();
+  //int N_TRIAL_PARAMS = get_n_trial_params();
+  //int N_TRIAL_RESULTS = get_n_trial_results();
+  char** param_abbrevs = get_param_abbrevs();
+  char** results_abbrevs = get_results_abbrevs();
+  bool* param_report_ET = get_param_report_ET();
+  long* default_results_values = get_default_results_values();  
+  
+  // Set up the trial based on received trial parameters
+  Serial.print(time);
+  Serial.println(" TRL_START");
+  for(int i=0; i < N_TRIAL_PARAMS; i++) {
+    if (param_report_ET[i]) {
+      // Buffered write would be nice here
+      Serial.print(time);
+      Serial.print(" TRLP ");
+      Serial.print(param_abbrevs[i]);
+      Serial.print(" ");
+      Serial.println(param_values[i]);
+    }
+  }
+
+  // Set up trial_results to defaults
+  for(int i=0; i < N_TRIAL_RESULTS; i++) {
+    results_values[i] = default_results_values[i];
+  }      
+  
+  // Run user-specific trial start code, including setting next state
+  return user_trial_start(time);
+}
+
+
+//// StateFinishTrial methods
+// Announces results on setup
+void StateFinishTrial::s_setup() {
+  // Get the values from Params.h
+  long* results_values = get_results_values();
+  //int N_TRIAL_RESULTS = get_n_trial_results();
+  char** results_abbrevs = get_results_abbrevs();
+  
+  // Announce results
+  for(int i=0; i < N_TRIAL_RESULTS; i++) {
+    Serial.print(time_of_last_call);
+    Serial.print(" TRLR ");
+    Serial.print(results_abbrevs[i]);
+    Serial.print(" ");
+    Serial.println(results_values[i]);
+  }
+}
+
+// Once the timer has elapsed, start the next trial.
+State* StateFinishTrial::s_finish() {
+  return state_wait_to_start_trial;   
+}
+
+
+//// Setup function
+void setup()
+{ /* Standard setup function to initialize the arduino.
+  
+  1. Initializes the serial port and announces time and (TODO) version info
+  2. Runs a protocol-specific user_setup1() function that sets things like
+     inputs/outpus before receiving any serial communcation.
+  3. Runs serial communication until enough info has been received to run
+     the first trial.
+  4. Runs a protocol-specific user_setup2() function that finalizes anything
+     that depens on receiving input, like 2-pin vs 4-pin mode.
+  */
+  unsigned long time = millis();
+  int status = 1;
+  
+  // Initalize serial port communication and announce time
+  Serial.begin(115200);
+  Serial.print(time);
+  Serial.println(" DBG begin setup");
+
+  // Protocol-specific setup1, to be run before receiving any serial data
+  user_setup1();
+
+  // Run communications until we've received enough startup info to
+  // start the first trial.
+  while (!flag_start_trial)
+  {
+    status = communications(time);
+    if (status != 0)
+    {
+      Serial.println("comm error in setup");
+      delay(1000);
+    }
+  }
+  
+  // Now finalize the setup using the received initial parameters
+  user_setup2();
+}
+
+
+//// Loop function
+void loop()
+{
+  // All serial communications are marked with this time, so get it right
+  // at the beginning
+  unsigned long time = millis();
+  
+  // Current state variable, persistent across loops
+  // The first time through, we want to initialize to wait_to_start_trial
+  static State* current_state = state_wait_to_start_trial;
+  static State* next_state = state_wait_to_start_trial;
+  
+  // misc
+  int status = 1;
+  
+  // Run communications
+  status = communications(time);
+  
+  // User-defined every-loop code
+  user_every_loop(time);
+  
+  // State
+  next_state = current_state->run(time);
+  
+  //// Update the state variable
+  if (next_state != current_state)
+  {
+    announce_state_change(time, current_state, next_state);
+  }
+  current_state = next_state;
+  
+  return;
+}
+
+
+//// Utility functions
+// Announce each state change
+// We print the time at the beginning of the loop call, the current and
+// next states, and the current time ("ST_CHG2"). The reason that we print
+// the time at the beginning and end of the loop call is to help estimate
+// the duration of certain states that take a long time (due to delay
+// statements).
+void announce_state_change(unsigned long time, State *current_state,
+  State *next_state)
+{
+  Serial.print(time);
+  Serial.print(" ST_CHG ");
+  Serial.print(current_state->id());
+  Serial.print(" ");
+  Serial.println(next_state->id());
+
+  Serial.print(millis());
+  Serial.print(" ST_CHG2 ");
+  Serial.print(current_state->id());
+  Serial.print(" ");
+  Serial.println(next_state->id());
+}
+
+
+
+
+
+
+
+
+
+
+
+//////////
 
 
 ///// STATES CPP
