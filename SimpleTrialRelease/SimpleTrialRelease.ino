e0689bc1dffad2c3035b72a89ff68558ef126472
Chris Rodgers <xrodgers@gmail.com>
2015-03-31 22:14:01 -0400

    adding rotation function and removing mpr121 and servo

--- SimpleTrialRelease/SimpleTrialRelease.ino
+++ SimpleTrialRelease/SimpleTrialRelease.ino
@@ -25,13 +25,14 @@ Additionally, you can provide the following:
 #include "States.h"
 #include "ArduFSM.h"
 #include "Stepper.h"
-#include "Servo.h"
-#include "mpr121.h"
-#include <Wire.h>
+//~ #include "Servo.h"
+//~ #include "mpr121.h"
+//~ #include <Wire.h>
 #include "hwconstants.h"
 
 Stepper *stimStepper = 0;
-Servo servo;
+//~ Servo servo;
+
 
 // Standard user_setup1() function, run before first communications.
 // This simple protocol doesn't require anything here.
@@ -49,14 +50,16 @@ void user_setup2() {
   
   stimStepper = new Stepper(__HWCONSTANTS_H_NUMSTEPS, 
       PIN_STEPPER1, PIN_STEPPER2, PIN_STEPPER3, PIN_STEPPER4);
-  servo.attach(LINEAR_SERVO);
+  //~ servo.attach(LINEAR_SERVO);
   // thresholds for MPR121
-  mpr121_setup(TOUCH_IRQ, 6, 6);
+  //~ mpr121_setup(TOUCH_IRQ, 6, 6);
 }
 
 // Standard user_every_loop() function, run on every loop
 void user_every_loop(unsigned long time) {
-  
+  Serial.println("DBG stepping");
+  delay(500);
+  rotate(1);
 }
 
 // Standard user_trial_start() function, run at beginning of every trial
@@ -66,4 +69,31 @@ State* user_trial_start(unsigned long time) {
     param_values[tpidx_ITI]);
   
   return state_wait;
+}
+
+
+
+int rotate(long n_steps)
+{ /* Low-level rotation function 
+  
+  I think positive n_steps means CCW and negative n_steps means CW. It does
+  on L2, at least.
+  */
+
+  digitalWrite(ENABLE_STEPPER, HIGH);
+Serial.println("DBG stepping0");
+  // Sometimes the stepper spins like crazy without a delay here
+  delay(__HWCONSTANTS_H_STP_POST_ENABLE_DELAY);
+  Serial.println("DBG stepping1");
+  // BLOCKING CALL //
+  // Replace this with more iterations of smaller steps
+  stimStepper->step(n_steps);
+Serial.println("DBG stepping2");
+  // This delay doesn't seem necessary
+  //delay(50);
+  
+Serial.println("DBG stepping3");
+  digitalWrite(ENABLE_STEPPER, LOW);
+  
+  return 0;
 }
