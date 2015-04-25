9ac782b8ec281fdf9fb2f52f8b7656b119c76231
Chris Rodgers <xrodgers@gmail.com>
2015-04-25 12:40:27 -0400

    i don't know

--- TwoChoice/TwoChoice.ino
+++ TwoChoice/TwoChoice.ino
@@ -585,6 +585,11 @@ void asynch_action_light_on()
 
 
 
+//////////
+
+
+
+
 
 //////////// Ardu FSM CPP
 
@@ -740,6 +745,21 @@ void setup()
   */
   unsigned long time = millis();
   int status = 1;
+
+  state_rotate_stepper1 = new StateRotateStepper1();
+  state_rotate_stepper2 = new StateRotateStepper2();
+  state_reward_l = new StateRewardL();
+  state_reward_r = new StateRewardR();
+  state_response_window = new StateResponseWindow(
+    param_values[tpidx_RESP_WIN_DUR]);
+  state_inter_rotation_pause = new StateInterRotationPause(50);
+  state_wait_for_servo_move = new StateWaitForServoMove(
+    param_values[tpidx_SRV_TRAVEL_TIME]);
+  state_error_timeout = new StateErrorTimeout(
+    param_values[tpidx_ERROR_TIMEOUT]);
+  state_post_reward_pause = new StatePostRewardPause(
+    param_values[tpidx_INTER_REWARD_INTERVAL]);
+  
   
   // Initalize serial port communication and announce time
   Serial.begin(115200);
@@ -860,25 +880,136 @@ void announce_state_change(unsigned long time, State *current_state,
 
 
 
-//////////
+
+
+
+// Standard user_setup1 function
+// Sets pins as inputs/outputs
+// Connects to MPR121 and servo
+void user_setup1() {
+  Serial.print(millis());
+  Serial.println(" DBG begin user_setup1");
+
+  // MPR121 touch sensor setup
+  pinMode(TOUCH_IRQ, INPUT);
+  digitalWrite(TOUCH_IRQ, HIGH); //enable pullup resistor
+  Wire.begin();
+  
+  // output pins
+  pinMode(L_REWARD_VALVE, OUTPUT);
+  pinMode(R_REWARD_VALVE, OUTPUT);
+  pinMode(__HWCONSTANTS_H_HOUSE_LIGHT, OUTPUT);
+  
+  // initialize the house light to ON
+  digitalWrite(__HWCONSTANTS_H_HOUSE_LIGHT, HIGH);
+  
+  // random number seed
+  randomSeed(analogRead(3));
+
+  // attach servo
+  //Servo* servo = get_servo();
+  servo.attach(LINEAR_SERVO);
+}
+
+// Standard user_setup2() function, run after first communications complete.
+// Sets up two-pin or four-pin stepper, sets thresholds for MPR121,
+// moves servo to initial position.
+void user_setup2() {
+  // Set up the stepper according to two-pin or four-pin mode
+  if (param_values[tpidx_2PSTP] == __TRIAL_SPEAK_YES)
+  { // Two-pin mode
+    pinMode(TWOPIN_ENABLE_STEPPER, OUTPUT);
+    pinMode(TWOPIN_STEPPER_1, OUTPUT);
+    pinMode(TWOPIN_STEPPER_2, OUTPUT);
+    
+    // Make sure it's off    
+    digitalWrite(TWOPIN_ENABLE_STEPPER, LOW); 
+    
+    // Initialize
+    stimStepper = new Stepper(__HWCONSTANTS_H_NUMSTEPS, 
+      TWOPIN_STEPPER_1, TWOPIN_STEPPER_2);
+  }
+  else
+  { // Four-pin mode
+    pinMode(ENABLE_STEPPER, OUTPUT);
+    pinMode(PIN_STEPPER1, OUTPUT);
+    pinMode(PIN_STEPPER2, OUTPUT);
+    pinMode(PIN_STEPPER3, OUTPUT);
+    pinMode(PIN_STEPPER4, OUTPUT);
+    digitalWrite(ENABLE_STEPPER, LOW); // # Make sure it's off
+    
+    stimStepper = new Stepper(__HWCONSTANTS_H_NUMSTEPS, 
+      PIN_STEPPER1, PIN_STEPPER2, PIN_STEPPER3, PIN_STEPPER4);
+  }
+  
+  // thresholds for MPR121
+  mpr121_setup(TOUCH_IRQ, param_values[tpidx_TOU_THRESH], 
+    param_values[tpidx_REL_THRESH]);
+
+  // Set the speed of the stepper
+  stimStepper->setSpeed(param_values[tpidx_STEP_SPEED]);
+  
+  // initial position of the stepper
+  sticky_stepper_position = param_values[tpidx_STEP_INITIAL_POS];
+  
+  // linear servo setup
+  //Servo* servo = get_servo();
+  servo.write(param_values[tpidx_SRV_FAR]);
+  delay(param_values[tpidx_SERVO_SETUP_T]);
+}
+
+// Standard user_every_loop() function, run on every loop
+void user_every_loop(unsigned long time) {
+  // Poll touch inputs and check whether it matches the last announced
+  // value of touched.
+  static uint16_t sticky_announced_touched = 0;
+  uint16_t touched = get_touch_status(1);
+  
+  // announce sticky
+  if (touched != sticky_announced_touched)
+  {
+    Serial.print(time);
+    Serial.print(" TCH ");
+    Serial.println(touched);
+    sticky_announced_touched = touched;
+  }   
+}
+
+// Standard user_trial_start() function, run at beginning of every trial
+State* user_trial_start(unsigned long time) {
+  // Update state timers
+  static_cast<TimedState *>(state_error_timeout)->set_duration(
+    param_values[tpidx_ERROR_TIMEOUT]);
+  static_cast<TimedState *>(state_response_window)->set_duration(
+    param_values[tpidx_RESP_WIN_DUR]);
+  static_cast<TimedState *>(state_wait_for_servo_move)->set_duration(
+    param_values[tpidx_SRV_TRAVEL_TIME]);
+  static_cast<TimedState *>(state_post_reward_pause)->set_duration(
+    param_values[tpidx_INTER_REWARD_INTERVAL]);
+  
+  return state_rotate_stepper1;
+}
+
+
+
+
+
+
 
 
 ///// STATES CPP
 
 //// Instantiate one of each state
-State* state_rotate_stepper1 = new StateRotateStepper1();
-State* state_rotate_stepper2 = new StateRotateStepper2();
-State* state_reward_l = new StateRewardL();
-State* state_reward_r = new StateRewardR();
-State* state_response_window = new StateResponseWindow(
-  param_values[tpidx_RESP_WIN_DUR]);
-State* state_inter_rotation_pause = new StateInterRotationPause(50);
-State* state_wait_for_servo_move = new StateWaitForServoMove(
-  param_values[tpidx_SRV_TRAVEL_TIME]);
-State* state_error_timeout = new StateErrorTimeout(
-  param_values[tpidx_ERROR_TIMEOUT]);
-State* state_post_reward_pause = new StatePostRewardPause(
-  param_values[tpidx_INTER_REWARD_INTERVAL]);
+
+State* state_rotate_stepper1 = 0;
+State* state_rotate_stepper2 = 0;
+State* state_reward_l = 0;
+State* state_reward_r = 0;
+State* state_response_window = 0;
+State* state_inter_rotation_pause = 0;
+State* state_wait_for_servo_move = 0;
+State* state_error_timeout = 0;
+State* state_post_reward_pause = 0;
 
 
 // Implementations of states used in TwoChoice
@@ -1210,125 +1341,6 @@ int rotate(long n_steps)
 
 
 ///// END STATES CPP
-
-
-
-
-
-
-
-
-
-
-
-// Standard user_setup1 function
-// Sets pins as inputs/outputs
-// Connects to MPR121 and servo
-void user_setup1() {
-  Serial.print(millis());
-  Serial.println(" DBG begin user_setup1");
-
-  // MPR121 touch sensor setup
-  pinMode(TOUCH_IRQ, INPUT);
-  digitalWrite(TOUCH_IRQ, HIGH); //enable pullup resistor
-  Wire.begin();
-  
-  // output pins
-  pinMode(L_REWARD_VALVE, OUTPUT);
-  pinMode(R_REWARD_VALVE, OUTPUT);
-  pinMode(__HWCONSTANTS_H_HOUSE_LIGHT, OUTPUT);
-  
-  // initialize the house light to ON
-  digitalWrite(__HWCONSTANTS_H_HOUSE_LIGHT, HIGH);
-  
-  // random number seed
-  randomSeed(analogRead(3));
-
-  // attach servo
-  //Servo* servo = get_servo();
-  servo.attach(LINEAR_SERVO);
-}
-
-// Standard user_setup2() function, run after first communications complete.
-// Sets up two-pin or four-pin stepper, sets thresholds for MPR121,
-// moves servo to initial position.
-void user_setup2() {
-  // Set up the stepper according to two-pin or four-pin mode
-  if (param_values[tpidx_2PSTP] == __TRIAL_SPEAK_YES)
-  { // Two-pin mode
-    pinMode(TWOPIN_ENABLE_STEPPER, OUTPUT);
-    pinMode(TWOPIN_STEPPER_1, OUTPUT);
-    pinMode(TWOPIN_STEPPER_2, OUTPUT);
-    
-    // Make sure it's off    
-    digitalWrite(TWOPIN_ENABLE_STEPPER, LOW); 
-    
-    // Initialize
-    stimStepper = new Stepper(__HWCONSTANTS_H_NUMSTEPS, 
-      TWOPIN_STEPPER_1, TWOPIN_STEPPER_2);
-  }
-  else
-  { // Four-pin mode
-    pinMode(ENABLE_STEPPER, OUTPUT);
-    pinMode(PIN_STEPPER1, OUTPUT);
-    pinMode(PIN_STEPPER2, OUTPUT);
-    pinMode(PIN_STEPPER3, OUTPUT);
-    pinMode(PIN_STEPPER4, OUTPUT);
-    digitalWrite(ENABLE_STEPPER, LOW); // # Make sure it's off
-    
-    stimStepper = new Stepper(__HWCONSTANTS_H_NUMSTEPS, 
-      PIN_STEPPER1, PIN_STEPPER2, PIN_STEPPER3, PIN_STEPPER4);
-  }
-  
-  // thresholds for MPR121
-  mpr121_setup(TOUCH_IRQ, param_values[tpidx_TOU_THRESH], 
-    param_values[tpidx_REL_THRESH]);
-
-  // Set the speed of the stepper
-  stimStepper->setSpeed(param_values[tpidx_STEP_SPEED]);
-  
-  // initial position of the stepper
-  sticky_stepper_position = param_values[tpidx_STEP_INITIAL_POS];
-  
-  // linear servo setup
-  //Servo* servo = get_servo();
-  servo.write(param_values[tpidx_SRV_FAR]);
-  delay(param_values[tpidx_SERVO_SETUP_T]);
-}
-
-// Standard user_every_loop() function, run on every loop
-void user_every_loop(unsigned long time) {
-  // Poll touch inputs and check whether it matches the last announced
-  // value of touched.
-  static uint16_t sticky_announced_touched = 0;
-  uint16_t touched = get_touch_status(1);
-  
-  // announce sticky
-  if (touched != sticky_announced_touched)
-  {
-    Serial.print(time);
-    Serial.print(" TCH ");
-    Serial.println(touched);
-    sticky_announced_touched = touched;
-  }   
-}
-
-// Standard user_trial_start() function, run at beginning of every trial
-State* user_trial_start(unsigned long time) {
-  // Update state timers
-  static_cast<TimedState *>(state_error_timeout)->set_duration(
-    param_values[tpidx_ERROR_TIMEOUT]);
-  static_cast<TimedState *>(state_response_window)->set_duration(
-    param_values[tpidx_RESP_WIN_DUR]);
-  static_cast<TimedState *>(state_wait_for_servo_move)->set_duration(
-    param_values[tpidx_SRV_TRAVEL_TIME]);
-  static_cast<TimedState *>(state_post_reward_pause)->set_duration(
-    param_values[tpidx_INTER_REWARD_INTERVAL]);
-  
-  return state_rotate_stepper1;
-}
-
-
 
 
 
