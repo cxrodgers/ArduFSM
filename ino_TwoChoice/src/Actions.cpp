38d2623633e36666d17b2321ccbaffecaeb4873c
Chris Rodgers <xrodgers@gmail.com>
2015-06-07 20:31:54 -0400

    ino tool version of TwoChoice, modifying gitignore accordingly

---
+++ ino_TwoChoice/src/Actions.cpp
@@ -0,0 +1,154 @@
+// Implementations of asynchronous actions for SimpleTrialRelease
+#include "Actions.h"
+#include "Arduino.h"
+#include "States.h"
+#include "chat.h"
+#include "Params.h"
+#include "hwconstants.h"
+#include "mpr121.h"
+
+//// Take protocol action based on user command (ie, setting variable)
+int take_action(char *protocol_cmd, char *argument1, char *argument2)
+{ /* Protocol action.
+  
+  Currently two possible actions:
+    if protocol_cmd == 'SET':
+      argument1 is the variable name. argument2 is the data.
+    if protocol_cmd == 'ACT':
+      argument1 is converted into a function based on a dispatch table.
+        REWARD_L : reward the left valve
+        REWARD_R : reward the right valve
+        REWARD : reward the current valve
+
+  This logic could be incorporated in TrialSpeak, but we would need to provide
+  the abbreviation, full name, datatype, and optional handling logic for
+  each possible variable. So it seems to make more sense here.
+  
+  Return values:
+  0 - command parsed successfully
+  2 - unimplemented protocol_cmd
+  4 - unknown variable on SET command
+  5 - data conversion error
+  6 - unknown asynchronous action
+  */
+  int status;
+  
+  //~ Serial.print("DBG take_action ");
+  //~ Serial.print(protocol_cmd);
+  //~ Serial.print("-");
+  //~ Serial.print(argument1);
+  //~ Serial.print("-");
+  //~ Serial.println(argument2);
+  
+  if (strncmp(protocol_cmd, "SET\0", 4) == 0)
+  {
+    // Find index into param_abbrevs
+    int idx = -1;
+    for (int i=0; i < N_TRIAL_PARAMS; i++)
+    {
+      if (strcmp(param_abbrevs[i], argument1) == 0)
+      {
+        idx = i;
+        break;
+      }
+    }
+    
+    // Error if not found, otherwise set
+    if (idx == -1)
+    {
+      Serial.print("ERR param not found ");
+      Serial.println(argument1);
+      return 4;
+    }
+    else
+    {
+      // Convert to int
+      status = safe_int_convert(argument2, param_values[idx]);
+
+      // Debug
+      //~ Serial.print("DBG setting var ");
+      //~ Serial.print(idx);
+      //~ Serial.print(" to ");
+      //~ Serial.println(argument2);
+
+      // Error report
+      if (status != 0)
+      {
+        Serial.println("ERR can't set var");
+        return 5;
+      }
+    }
+  }   
+
+  else if (strncmp(protocol_cmd, "ACT\0", 4) == 0)
+  {
+    // Dispatch
+    if (strncmp(argument1, "REWARD_L\0", 9) == 0) {
+      asynch_action_reward_l();
+    } else if (strncmp(argument1, "REWARD_R\0", 9) == 0) {
+      asynch_action_reward_r();
+    } else if (strncmp(argument1, "REWARD\0", 7) == 0) {
+      asynch_action_reward();
+    } else if (strncmp(argument1, "THRESH\0", 7) == 0) {
+      asynch_action_set_thresh();
+    } else if (strncmp(argument1, "HLON\0", 5) == 0) {
+      asynch_action_light_on();
+    } 
+    else
+      return 6;
+  }      
+  else
+  {
+    // unknown command
+    return 2;
+  }
+  return 0;
+}
+
+
+void asynch_action_reward_l()
+{
+  unsigned long time = millis();
+  Serial.print(time);
+  Serial.println(" EV AAR_L");
+  digitalWrite(L_REWARD_VALVE, HIGH);
+  delay(param_values[tpidx_REWARD_DUR_L]);
+  digitalWrite(L_REWARD_VALVE, LOW); 
+}
+
+void asynch_action_reward_r()
+{
+  unsigned long time = millis();
+  Serial.print(time);
+  Serial.println(" EV AAR_R");
+  digitalWrite(R_REWARD_VALVE, HIGH);
+  delay(param_values[tpidx_REWARD_DUR_R]);
+  digitalWrite(R_REWARD_VALVE, LOW); 
+}
+
+void asynch_action_reward()
+{
+  if (param_values[tpidx_REWSIDE] == __TRIAL_SPEAK_CHOICE_LEFT)
+    asynch_action_reward_l();
+  else if (param_values[tpidx_REWSIDE] == __TRIAL_SPEAK_CHOICE_RIGHT)
+    asynch_action_reward_r();
+  else
+    Serial.println("ERR unknown rewside");
+}
+
+void asynch_action_set_thresh()
+{
+  unsigned long time = millis();
+  Serial.print(time);
+  Serial.println(" EV AAST");
+  mpr121_setup(TOUCH_IRQ, param_values[tpidx_TOU_THRESH], 
+    param_values[tpidx_REL_THRESH]);
+}
+
+void asynch_action_light_on()
+{
+  unsigned long time = millis();
+  Serial.print(time);
+  Serial.println(" EV HLON");
+  digitalWrite(__HWCONSTANTS_H_HOUSE_LIGHT, HIGH);
+}
---
+++ ino_TwoChoice/src/main.py
@@ -0,0 +1,173 @@
+# Main script to run to run TwoChoice_v2 behavior
+# Timings: set 'serial_timeout' in chatter, and 'timeout' in UI, to be low
+# enough that the response is quick, but not so low that it takes up all the
+# CPU power.
+# Right now, it does this on each loop
+# * Update chatter (timeout)
+# * Update UI (timeout)
+# * do other things, like reading logfile and setting next trial
+# So, if the timeouts are too low, it spends a lot more time reading the
+# logfile and there is more overhead overall.
+
+import sys
+import os
+import numpy as np, pandas
+import my
+import time
+import curses
+import matplotlib.pyplot as plt
+
+# Ardu imports. Because this is an example script that will be run in another
+# directory, don't assume we can import TrialSpeak et al directly
+import ArduFSM
+from ArduFSM import TrialSpeak, TrialMatrix
+from ArduFSM import trial_setter_ui
+from ArduFSM import Scheduler
+from ArduFSM import trial_setter
+from ArduFSM import mainloop
+
+
+## Find out what rig we're in using the current directory
+this_dir_name = os.getcwd()
+rigname = os.path.split(this_dir_name)[1]
+if rigname == 'TwoChoice' or rigname =='src':
+    # we're running in the github directory
+    rigname = 'L0'
+serial_port = mainloop.get_serial_port(rigname)
+#~ serial_port = '/dev/ttyACM1'
+
+## Get params
+params_table = mainloop.get_params_table()
+params_table = mainloop.assign_rig_specific_params(rigname, params_table)
+params_table.loc['RWIN', 'init_val'] = 1000
+params_table.loc['SRVTT', 'init_val'] = 1000
+params_table['current-value'] = params_table['init_val'].copy()
+
+## Get trial types
+if rigname in ['L1', 'L2', 'L3']:
+    trial_types = mainloop.get_trial_types('trial_types_3srvpos')
+else:
+    trial_types = mainloop.get_trial_types('trial_types_4srvpos')
+
+## Initialize the scheduler
+scheduler = Scheduler.SessionStarter(trial_types=trial_types)
+scheduler = Scheduler.Auto(trial_types=trial_types)
+
+## Create Chatter
+logfilename = 'out.log'
+logfilename = None # autodate
+chatter = ArduFSM.chat.Chatter(to_user=logfilename, to_user_dir='./logfiles',
+    baud_rate=115200, serial_timeout=.1, serial_port=serial_port)
+logfilename = chatter.ofi.name
+
+## Trial setter
+ts_obj = trial_setter.TrialSetter(chatter=chatter, 
+    params_table=params_table,
+    scheduler=scheduler)
+
+## Initialize UI
+RUN_UI = True
+RUN_GUI = True
+ECHO_TO_STDOUT = not RUN_UI
+if RUN_UI:
+    ui = trial_setter_ui.UI(timeout=100, chatter=chatter, 
+        logfilename=logfilename,
+        ts_obj=ts_obj)
+
+    try:
+        ui.start()
+
+    except curses.error as err:
+        raise Exception(
+            "UI error. Most likely the window is, or was, too small.\n"
+            "Quit Python, type resizewin to set window to 80x23, and restart.")
+
+    except:
+        print "error encountered when starting UI"
+        raise
+    
+    finally:
+        ui.close()
+
+## Main loop
+final_message = None
+try:
+    ## Initialize GUI
+    if RUN_GUI:
+        plotter = ArduFSM.plot.PlotterWithServoThrow(trial_types)
+        plotter.init_handles()
+        last_updated_trial = 0
+    
+    while True:
+        ## Chat updates
+        # Update chatter
+        chatter.update(echo_to_stdout=ECHO_TO_STDOUT)
+        
+        # Read lines and split by trial
+        # Could we skip this step if chatter reports no new device lines?
+        logfile_lines = TrialSpeak.read_lines_from_file(logfilename)
+        splines = TrialSpeak.split_by_trial(logfile_lines)
+
+        # Run the trial setting logic
+        translated_trial_matrix = ts_obj.update(splines, logfile_lines)
+        
+        ## Meta-scheduler
+        # Not sure how to handle this yet. Probably should be an object
+        # ts_obj.meta_scheduler that can change ts_obj.scheduler, or a method
+        # meta_scheduler(trial_matrix) that returns the new scheduler
+        # Actually, probably makes the most sense to have each meta-scheduler
+        # just be a scheduler called "auto" or something.
+        if len(translated_trial_matrix) > 8 and ts_obj.scheduler.name == 'session starter':
+            new_scheduler = Scheduler.ForcedAlternation(trial_types=trial_types)
+            ts_obj.scheduler = new_scheduler
+        
+        ## Update UI
+        if RUN_UI:
+            ui.update_data(logfile_lines=logfile_lines)
+            ui.get_and_handle_keypress()
+
+        ## Update GUI
+        # Put this in it's own try/except to catch plotting bugs
+        if RUN_GUI:
+            if last_updated_trial < len(translated_trial_matrix):
+                # update plot
+                plotter.update(logfilename)     
+                last_updated_trial = len(translated_trial_matrix)
+                
+                # don't understand why these need to be here
+                plt.show()
+                plt.draw()
+
+
+except KeyboardInterrupt:
+    print "Keyboard interrupt received"
+
+except trial_setter_ui.QuitException as qe:
+    final_message = qe.message
+
+except curses.error as err:
+    raise Exception(
+        "UI error. Most likely the window is, or was, too small.\n"
+        "Quit Python, type resizewin to set window to 80x23, and restart.")
+
+except:
+    raise
+
+finally:
+    chatter.close()
+    print "chatter closed"
+    
+    if RUN_UI:
+        ui.close()
+        print "UI closed"
+    
+    if RUN_GUI:
+        pass
+        #~ plt.close(plotter.graphics_handles['f'])
+        #~ print "GUI closed"
+    
+    if final_message is not None:
+        print final_message
+    
+
+
