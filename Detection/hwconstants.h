3d63179ed8962de574b384021304d4e4256292b7
Chris Rodgers <xrodgers@gmail.com>
2016-01-04 19:01:39 -0500

    adding Detection task

---
+++ Detection/hwconstants.h
@@ -0,0 +1,20 @@
+#ifndef __HWCONSTANTS_H_INCLUDED__
+#define __HWCONSTANTS_H_INCLUDED__
+
+// Digital pins
+#define motorPin1 8
+#define motorPin2 6
+#define enablePin 7
+#define startPin 2
+#define optoPin 13
+#define leverPin 9
+#define rewPin 5
+#define speakerPin 11
+
+// Analog
+#define hallPin 1
+
+// Other constants
+#define stepsPerRevolution 200
+
+#endif
