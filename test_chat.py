d488b2273926db18cd4369f3fe189a1662362335
danieldkato <daniel.d.kato@gmail.com>
2016-03-21 11:18:24 -0400

    Deleted stale ardulines files

--- test_chat.py
+++ test_chat.py
@@ -1,28 +1,27 @@
-import serial
-import time
-import datetime
-import os
-import sys
-import errno
-import platform
 import chat 
 
-chtr = chat.Chatter('COM3')
+portNum = 3
+serialPort = 'COM' + str(portNum)
 testMessage = 'test test test\n'
+
+#Create chatter object
+chtr = chat.Chatter(serialPort)
  
+#Write the test message to the Chatter object's pipein
 f = open(chtr.pipein.name, 'w')
 f.write(testMessage)
 f.close()
  
+#The next 4 lines replicate the Chatter.update() function 
 new_user_text = chat.read_from_user(chtr.pipein)
 chat.write_to_device(chtr.ser, new_user_text)
  
 new_device_lines = chat.read_from_device(chtr.ser)
 chat.write_to_user(chtr.ofi, new_device_lines)
  
+#Read the message read back from the Arduino
 f2 = open(chtr.ofi.name, 'r')
 echo = f2.read()
-f2.close()
- 
+f2.close() 
 print( 'Received echo = ' + echo)
  
