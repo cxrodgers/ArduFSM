be6dd480b7aafc87152660e06646ba4b5229adb7
danieldkato <daniel.d.kato@gmail.com>
2016-06-09 15:07:28 -0400

    Edit comments for config.cpp

--- MultiSens/config.cpp
+++ MultiSens/config.cpp
@@ -1,3 +1,16 @@
+/* Last updated DDK 6/7/16
+ * 
+ * OVERVIEW:
+ * This file contains instantiations of the device objects to be used on the current 
+ * experiment of the ArduFSM protocol MultiSens. These objects manage the behavior
+ * of devices like stepper motors, speakers, etc. For full documentation of 
+ * these device classes, see the README.md for the devices library. 
+ * 
+ * This file also places the device objects in an array and returns the array
+ * to States.cpp, precluding the need to adjust hardware parameters in States.cpp
+ * from experiment to experiment.
+ */
+
 #include "config.h"
 
 Device ** config_hw(){ 
