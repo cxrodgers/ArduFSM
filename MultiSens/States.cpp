17e989f8d955fe8c38a18a1564e62b9db4ca1e11
danieldkato <daniel.d.kato@gmail.com>
2016-06-29 13:51:50 -0400

    Move definition of config_hw from config.cpp to States.cpp

--- MultiSens/States.cpp
+++ MultiSens/States.cpp
@@ -455,3 +455,24 @@ boolean checkLicks(){
     }
   return licking;
 }
+
+
+Device ** config_hw(){ 
+    
+    bool debug = 1; // set to 0 if using real hardware devices; set to 1 if using dummy devices
+    
+    if ( debug == 0 ){
+        static myStepper myStp1( NUM_STEPS, STPR1_PIN1, STPR1_PIN1, ENBL1_PIN, HALL1_PIN, HALL1_THRESH, STPR1_SPEED, STPR1_CW, STPR1_CCW, HALL1_VAL );  
+        static mySpeaker spkr1( SPKR_PIN );
+  
+        static Device * devPtrs[] = { &myStp1, &spkr1 };
+        return devPtrs;
+        }
+    else {
+        static dummyStepper dmStpr1;
+        static dummySpeaker dmSpkr1;
+  
+        static Device * devPtrs[] = { &dmStpr1, &dmSpkr1 };
+        return devPtrs;
+    }    
+}
