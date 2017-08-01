b60249ba45359cd2cd05453600db33369c2cc02a
danieldkato <daniel.d.kato@gmail.com>
2017-08-01 16:02:17 -0400

    Make test_get_SHA1.py get SHA1 hash of any libraries used by main sketch

--- MultiSens/test_get_SHA1.py
+++ MultiSens/test_get_SHA1.py
@@ -15,10 +15,10 @@ baseCmd = 'git log -n 1 --pretty=format:%H -- '
 for s in sources:
 	
 	# get path:
-	fullPath = ' -- ' + baseDir + '\\' + s
+	fullPath = baseDir + '\\' + s
 	
 	# get SHA1 hash:
-	fullCmd = baseCmd + fullPath
+	fullCmd = baseCmd + ' -- ' + fullPath
 	proc = subprocess.Popen(fullCmd, stdout=subprocess.PIPE, shell=True)
 	sha1, err = proc.communicate()
 	
@@ -29,11 +29,15 @@ for s in sources:
 # proc = subprocess.Popen(cmd, stdout = subprocess.PIPE, shell=True)
 
 # find all libraries used by the uploaded sketch:
+arduinoPath = 'C:\\Program Files (x86)\\Arduino'
 libBaseStr = 'Using library'
-inos = [y for y in sources if '.ino' in y]
+inos = [y for y in os.listdir(os.getcwd()) if '.ino' in y]
+os.chdir(arduinoPath)
 verifyCmd = 'arduino_debug -v --verify ' + baseDir + '\\' + inos[0]
 out, err = subprocess.Popen(verifyCmd, stdout=subprocess.PIPE, shell=True).communicate()
+out = out.splitlines()
 libLines = [z for z in out if libBaseStr in z]
+print(libLines)
 
 # create a dict for each library used by the main sketch:
 libList = []
@@ -41,21 +45,37 @@ for lib in libLines:
 	
 	# get the path of the library:
 	ind = lib.find(':')
-	libPath = lib[ind:]
+	libPath = lib[ind+2:]
+	print(libPath)
 	
 	# if the line ends in '(legacy)' because it's been previously compiled, exclude '(legacy)' to isolate the path to the library
 	if libPath[-len(' (legacy)'):]  == ' (legacy)':
 		libPath = libPath[:-len(' (legacy)')]
+	print(libPath)
 	
 	# get the SHA1 hash of the library:
+	os.chdir(libPath)
+	
+	try:
+		sha1 = subprocess.check_output(['git', 'log', '-n 1', '--pretty=format:%H'], stderr=subprocess.STDOUT)
+	except subprocess.CalledProcessError as e:
+		print('output:')
+		print(e.output)
+		sha1 = e.output
+	
+	"""
 	proc2 = subprocess.Popen(baseCmd, stdout=subprocess.PIPE, shell=True)
-	sha1, err = proc.communicate()
+	sha1, err = proc2.communicate()
+	print('return:')
+	print(sha1)
+	"""
 
 	# put path and SHA1 hash into dict:
-	e = {"path": fullPath, "SHA1": sha1}
+	e = {"path": libPath, "SHA1": sha1}
 	libList.append(e)
 
 # Place list of dicts into dict object, then write to JSON file
 out = {'srcFiles': srcList, 'libraries': libList}
+os.chdir(baseDir)
 with open('testJSON.json', 'w') as fp:
 	json.dump(out, fp)
