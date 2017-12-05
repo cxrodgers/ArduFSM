e081cbcd39930c0d9c40989e8338b88a92b2fb03
Chris Rodgers <xrodgers@gmail.com>
2017-12-04 21:07:08 -0500

    adding new version of Detection task for December 2017 expts

---
+++ Detection2b/hwconstants.h
@@ -0,0 +1,24 @@
+#ifndef __HWCONSTANTS_H_INCLUDED__
+#define __HWCONSTANTS_H_INCLUDED__
+
+// Digital pins
+#define motorPin1 8
+#define motorPin2 6
+#define enablePin 7
+#define startPin 2
+#define optoPin 13
+#define rewPin 5
+#define speakerPin 11
+
+// New digital pin as of 2017
+#define lampPin 4
+
+// Analog
+// leverPin used to be DIO9
+#define hallPin 1
+#define leverPin 9
+
+// Other constants
+#define stepsPerRevolution 200
+
+#endif
