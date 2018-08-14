3d26b23346134127b932df8da904ebb33b88191d
Dan <daniel.d.kato@gmail.com>
2018-08-14 17:40:14 -0400

    In States.cpp, set stepper_powerdown_time to 200 milliseconds

--- MultiSens/States.cpp
+++ MultiSens/States.cpp
@@ -75,7 +75,7 @@ int numSteps = floor((REVERSE_ROTATION_DEGREES/360.0) * NUM_STEPS) * MICROSTEP;
 String stprState = "RETRACTED";
 int trial_start_signal_duration = 50; 
 int stpr_powerup_time = 150; // number of milliseconds between when the stepper driver is enabled and when the step signal is sent; this is necessary to ensure that stepper actually stops
-int stpr_powerdown_time = 150; // found empirically that a longer power-down time results in less variance in the stop position of the stepper
+int stpr_powerdown_time = 200; // found empirically that a longer power-down time results in less variance in the stop position of the stepper
 
 // These should go into some kind of Protocol.h or something
 char* param_abbrevs[N_TRIAL_PARAMS] = {
