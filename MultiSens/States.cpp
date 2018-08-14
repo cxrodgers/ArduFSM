6ccd4f54da20fd25ab47f8370c29b07ffb2e5906
Dan <daniel.d.kato@gmail.com>
2018-08-14 17:13:54 -0400

    In States.cpp, add a separate, longer stepper_powerdown_time variable

--- MultiSens/States.cpp
+++ MultiSens/States.cpp
@@ -75,6 +75,7 @@ int numSteps = floor((REVERSE_ROTATION_DEGREES/360.0) * NUM_STEPS) * MICROSTEP;
 String stprState = "RETRACTED";
 int trial_start_signal_duration = 50; 
 int stpr_powerup_time = 150; // number of milliseconds between when the stepper driver is enabled and when the step signal is sent; this is necessary to ensure that stepper actually stops
+int stpr_powerdown_time = 150; // found empirically that a longer power-down time results in less variance in the stop position of the stepper
 
 // These should go into some kind of Protocol.h or something
 char* param_abbrevs[N_TRIAL_PARAMS] = {
@@ -275,7 +276,7 @@ void StimPeriod::s_setup(){
       trigger_audio();
       delay(param_values[tpidx_INTERSTIM_LATENCY]);
       trigger_stepper();
-      delay(stpr_powerup_time);
+      delay(stpr_powerdown_time);
       digitalWrite(ENBL_PIN, HIGH);
     }  else {
       if (param_values[tpidx_STPRIDX] == 1){
@@ -286,7 +287,7 @@ void StimPeriod::s_setup(){
       trigger_stepper();
       delay(-1 * param_values[tpidx_INTERSTIM_LATENCY]);
       trigger_audio();
-      delay(stpr_powerup_time);
+      delay(stpr_powerdown_time);
       digitalWrite(ENBL_PIN, HIGH);
     } 
 
@@ -327,7 +328,7 @@ void StimPeriod::s_finish()
        digitalWrite(ENBL_PIN, LOW);
        delay(stpr_powerup_time);
        rotate_back();
-       delay(stpr_powerup_time);
+       delay(stpr_powerdown_time);
        digitalWrite(ENBL_PIN, HIGH);
    }
     
